afa930d7de10a19af6cfd4cfdfec2d6adc48801c
Lily Comeau <133320631+lcomeau01@users.noreply.github.com>
2024-04-20 03:11:08 -0400

    Update stock2.js

--- stock2.js
+++ stock2.js
@@ -9,6 +9,7 @@ html_form += "</form>";
 
 var http = require('http'); 
 var url = require('url'); 
+var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) { 
     res.writeHead(200, {'Content-Type': 'text/html'}); 
